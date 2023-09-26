14ab9cc1f600f304eb4b11a02e0a1340bdfff293
Bruno <Cliente@DESKTOP-2TBK80U>
2023-09-26 17:41:13 -0300

    implementado a parte de detalhes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarFilmesComponent } from './components/listar-filmes/listar-filmes.component';
+import { DetalhesFilmeComponent } from './components/detalhes-filme/detalhes-filme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-filmes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'listar-filmes',
+    component: ListarFilmesComponent,
+  },
+  {
+    path: 'detalhes-filmes/:id',
+    component: DetalhesFilmeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
